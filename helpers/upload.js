751f4d7b95f58e65e03ee251f0c6f84c641d86c4
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-03-14 00:23:48 +0200

    hw5 pt3

--- helpers/upload.js
+++ helpers/upload.js
@@ -1,9 +1,14 @@
 import multer from "multer";
 import * as path from "node:path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, path.join(process.cwd(), "tmp"));
+    const destinationPath = path.join(process.cwd(), "tmp");
+    if (!fs.existsSync(destinationPath)) {
+      fs.mkdirSync(destinationPath, { recursive: true });
+    }
+    cb(null, destinationPath);
   },
   filename(req, file, cb) {
     cb(null, file.originalname);
